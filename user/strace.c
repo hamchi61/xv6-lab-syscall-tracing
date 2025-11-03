60198c00cc698b55b4dc4701e040b43a0aaf604f
hamchi61 <won.mei.0601@gmail.com>
2025-11-03 01:33:48 +0000

    step5

---
+++ user/strace.c
@@ -0,0 +1,25 @@
+//STEP5
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        exit(1);
+    }
+
+    int pid = fork();
+
+    if (pid == 0) {
+        exec(argv[1], &argv[1]);
+        exit(1);
+    } else {
+        if (trace(pid) < 0) {
+            exit(1);
+        }
+        wait(0);
+    }
+
+    exit(0);
+}
